0a27a915c3cc09356aada9777974b1bbf4a2ff98
Synth <synth9283@gmail.com>
2021-08-22 18:23:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 mv -v ~/.Xresources ~/.Xresources_bak
 mv -v .Xresources ~/.Xresources
-mv -v /usr/lib/X11/xdm/wallpaper.jpg /usr/lib/X11/xdm/wallpaper.jpg_bak
-mv -v wallpaper.jpg /usr/lib/X11/xdm/wallpaper.jpg
+mv -v /usr/share/sxmo/background.jpg /usr/share/sxmo/background.jpg_bak
+mv -v background.jpg /usr/share/sxmo/background.jpg
