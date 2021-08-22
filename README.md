2742eb4532f2cbcd8faf2bc0ce4aef451efce495
Synth <synth9283@gmail.com>
2021-08-22 18:20:19 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # synth-sxmo
 My personal settings for sxmo
+
+# Notes
+```
+mv .Xresources ~/.Xresources
+mv wallpaper.jpg /usr/lib/X11/xdm/wallpaper.jpg
+```
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv -v ~/.Xresources ~/.Xresources_bak
+mv -v .Xresources ~/.Xresources
+mv -v /usr/lib/X11/xdm/wallpaper.jpg /usr/lib/X11/xdm/wallpaper.jpg_bak
+mv -v wallpaper.jpg /usr/lib/X11/xdm/wallpaper.jpg
