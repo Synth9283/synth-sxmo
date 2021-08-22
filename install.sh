cad5244d7bff43539053d6cbe7162272104b4d9a
Synth <synth9283@gmail.com>
2021-08-22 18:31:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cp -v ~/.Xresources ~/.Xresources_bak
 cp -v .Xresources ~/.Xresources
-cp -v /usr/share/sxmo/background.jpg /usr/share/sxmo/background.jpg_bak
-cp -v background.jpg /usr/share/sxmo/background.jpg
+sudo cp -v /usr/share/sxmo/background.jpg /usr/share/sxmo/background.jpg_bak
+sudo cp -v background.jpg /usr/share/sxmo/background.jpg
