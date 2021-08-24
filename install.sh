60cfd19f69602130223341ae71b35171e37a57ef
Synth <synth9283@gmail.com>
2021-08-24 08:14:15 -0400

    Adds permissions to userscripts

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ cp -v ~/.Xresources ~/.Xresources_bak
 cp -v .Xresources ~/.Xresources
 sudo cp -v /usr/share/sxmo/background.jpg /usr/share/sxmo/background.jpg_bak
 sudo cp -v background.jpg /usr/share/sxmo/background.jpg
+chmod +x userscripts/*.sh
 cp -v userscripts/*.sh $XDG_CONFIG_HOME/sxmo/userscripts/
