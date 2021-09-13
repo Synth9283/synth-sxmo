544a3ff0b5d5229bb779816a7a28096e12b3700a
Synth <synth9283@gmail.com>
2021-09-13 18:24:15 -0400

    feat: Added script to generate userscript for each anbox app

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@ cp -v .Xresources ~/.Xresources
 sudo cp -v /usr/share/sxmo/background.jpg /usr/share/sxmo/background.jpg_bak
 sudo cp -v background.jpg /usr/share/sxmo/background.jpg
 cp -v userscripts/*.sh $XDG_CONFIG_HOME/sxmo/userscripts/
+
+for file in ~/.local/share/applications/anbox/*.desktop;
+do
+    appName=$(cat $file | grep "Name=" | cut -d'=' -f 2)
+    execCommand=$(cat $file | grep "Exec=" | cut -d'=' -f 2-)
+
+    echo -e "#!/bin/bash\n" > $XDG_CONFIG_HOME/sxmo/$appName.sh
+    echo "export EGL_PLATFORM=x11" >> $XDG_CONFIG_HOME/sxmo/$appName.sh
+    echo $execCommand >> $XDG_CONFIG_HOME/sxmo/$appName.sh
+done
+
 chmod +x $XDG_CONFIG_HOME/sxmo/userscripts/*.sh
