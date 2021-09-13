a6930ff161996c1c7b69a0246f82495dad28247a
Synth <synth9283@gmail.com>
2021-09-13 18:35:50 -0400

    fix: Fixed wrong path and filtered out default apps

--- install.sh
+++ install.sh
@@ -8,12 +8,18 @@ cp -v userscripts/*.sh $XDG_CONFIG_HOME/sxmo/userscripts/
 
 for file in ~/.local/share/applications/anbox/*.desktop;
 do
+    defaultAppName="Calculator Calendar Clock Contacts Email F-Droid Files Gallery Music Settings Simple Keyboard WebView WhatsApp"
     appName=$(cat $file | grep "Name=" | cut -d'=' -f 2)
     execCommand=$(cat $file | grep "Exec=" | cut -d'=' -f 2-)
 
-    echo -e "#!/bin/bash\n" > $XDG_CONFIG_HOME/sxmo/$appName.sh
-    echo "export EGL_PLATFORM=x11" >> $XDG_CONFIG_HOME/sxmo/$appName.sh
-    echo $execCommand >> $XDG_CONFIG_HOME/sxmo/$appName.sh
+    if echo $defaultAppName | grep -w $appName > /dev/null; then
+        continue
+    else
+
+        echo -e "#!/bin/bash\n" > $XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh
+        echo "export EGL_PLATFORM=x11" >> $XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh
+        echo $execCommand >> $XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh
+    fi
 done
 
 chmod +x $XDG_CONFIG_HOME/sxmo/userscripts/*.sh
