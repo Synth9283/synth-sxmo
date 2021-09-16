848bb7c9ecbcb7471a2d5f1804e219cd33490fd4
Synth <synth9283@gmail.com>
2021-09-16 17:47:00 -0400

    fix: install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cp -v userscripts/*.sh $XDG_CONFIG_HOME/sxmo/userscripts/
 for file in ~/.local/share/applications/anbox/*.desktop;
 do
     defaultAppName="Calculator Calendar Clock Contacts Email F-Droid Files Gallery Music Settings Simple Keyboard WebView"
-    appName=$(cat $file | grep "Name=" | cut -d'=' -f 2)
+    appName=$(cat $file | grep "Name=" | cut -d'=' -f 2-)
     execCommand=$(cat $file | grep "Exec=" | cut -d'=' -f 2-)
 
     if echo $defaultAppName | grep -w $appName > /dev/null; then
@@ -18,7 +18,7 @@ do
 
         echo -e "#!/bin/bash\n" > "$XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh"
         echo "export EGL_PLATFORM=x11" >> "$XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh"
-        echo $execCommand >> "$XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh"
+        echo "$execCommand" >> "$XDG_CONFIG_HOME/sxmo/userscripts/$appName.sh"
     fi
 done
 
