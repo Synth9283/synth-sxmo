70bb7c69ddb5f869acbaf8870be4d361ac9b6ac2
Synth <synth9283@gmail.com>
2021-10-16 21:38:52 -0400

    fix: Color mint

--- dwm-config.def.h
+++ dwm-config.def.h
@@ -15,10 +15,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_mint[]        = "#3eb489";
 static const char *defaultcolors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_mint,  col_mint  },
 	[SchemeTabActive]  = { col_gray2, col_gray3,  col_gray2 },
 	[SchemeTabInactive]  = { col_gray1, col_gray3,  col_gray1 }
 };
@@ -79,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_mint, "-sf", col_gray4, NULL };
 
 #include <X11/XF86keysym.h>
 static Key keys[] = {
